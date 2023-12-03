dfed83c519d14104c545203867bf33d2209d44fc
Adeyinka Adebayo A <adeyinkaadebayo032@gmail.com>
2023-12-03 10:24:34 +0100

    first cmmit

---
+++ index.js
@@ -0,0 +1,15 @@
+  // Scroll reveal for the whole body
+  ScrollReveal({
+    reset: true,
+    distance: "150px",
+    duration: 2500,
+    delay: 400
+  });
+  
+
+  ScrollReveal().reveal('.stagger',{delay: 200, origin: 'bottom', interval: 500});
+
+
+  ScrollReveal().reveal('.from-right',{delay: 300, origin: 'right', });
+  ScrollReveal().reveal('.from-left ',{delay: 400, origin: 'left',});
+
